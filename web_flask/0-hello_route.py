86371e87cafe195db843c467eaa5dbca0d8fdde3
EbenGitHub <abenezergoo@gmail.com>
2022-10-15 01:01:33 +0300

    edt fl

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,3 +1,9 @@
+#!/usr/bin/python3
+'''
+Starts the flask application
+the flask runs on 0.0.0.0 on port 5000
+the home page should return hello hbnb
+'''
 from flask import Flask
 
 app = Flask(__name__)
@@ -5,4 +11,9 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def index():
+    '''
+    the home page and return hello hbnb
+    '''
     return "Hello HBNB!"
+
+app.run(host="0.0.0.0")
